9a845f185680f33ad096d020799d7d54ac1d2f79
ChihMin <s102062111@m102.nthu.edu.tw>
2015-06-23 17:10:35 +0800

    add motor arduino code

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,89 +1,74 @@
-// This Arduino example demonstrates bidirectional operation of a 
-// 28BYJ-48, using a ULN2003 interface board to drive the stepper.
-// The 28BYJ-48 motor is a 4-phase, 8-beat motor, geared down by
-// a factor of 68. One bipolar winding is on motor pins 1 & 3 and
-// the other on motor pins 2 & 4. The step angle is 5.625/64 and the 
-// operating Frequency is 100pps. Current draw is 92mA. 
-////////////////////////////////////////////////
 #include <SoftwareSerial.h>
 
-SoftwareSerial BT(10, 9);
+SoftwareSerial BT(12, 11); // recev , trans // recev , trans
 char val;
-const byte ledPin = 8;
 
-//declare variables for the motor pins
-int motorPin1 = 4;    // Blue   - 28BYJ48 pin 1
-int motorPin2 = 5;    // Pink   - 28BYJ48 pin 2
-int motorPin3 = 6;    // Yellow - 28BYJ48 pin 3
-int motorPin4 = 7;    // Orange - 28BYJ48 pin 4
-                        // Red    - 28BYJ48 pin 5 (VCC)
+const byte IN1 = 6;
+const byte IN2 = 7;
+const byte IN3 = 8;
+const byte IN4 = 9;
 
-int motorSpeed = 2400;  //variable to set stepper speed
-int count = 0;          // count of steps made
-int countsperrev = 512; // number of steps per full revolution
-int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
-
-//////////////////////////////////////////////////////////////////////////////
 void setup() {
-  //declare the motor pins as outputs
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  pinMode(motorPin3, OUTPUT);
-  pinMode(motorPin4, OUTPUT);
-
-  pinMode(ledPin, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+ // Serial.begin(9600);
   BT.begin(9600);
-  BT.print("BT is ready!");
-  Serial.begin(9600);
 }
 
-//////////////////////////////////////////////////////////////////////////////
+void forward_A(){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
+
+void backward_A(){
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN1, LOW);
+}
+
+void forward_B(){
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backward_B(){
+  digitalWrite(IN4, HIGH);
+  digitalWrite(IN3, LOW);
+}
+
+void stop_A_B(){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
 void loop(){
 
-  if(count < countsperrev )
-    clockwise();
-  else if (count == countsperrev * 2)
-    count = 0;
-  else
-    anticlockwise();
-  count++;
-/*
-  if(BT.available()){
-    char val = BT.read();
-    if (val == '1')
-      clockwise();
-    else
-      anticlockwise();
-      
-  }
- */
-}
+  if (BT.available()){
+    val = BT.read();
+    if (val == 'w'){
+      forward_A();
+      forward_B();
+    }
+    else if (val == 'a'){
+      backward_A();
+      forward_B();
+    }
 
-//////////////////////////////////////////////////////////////////////////////
-//set pins to ULN2003 high in sequence from 1 to 4
-//delay "motorSpeed" between each pin setting (to determine speed)
-void anticlockwise()
-{
-  for(int i = 0; i < 8; i++)
-  {
-    setOutput(i);
-    delayMicroseconds(motorSpeed);
-  }
-}
+    else if (val == 's'){
+      backward_A();
+      backward_B();
+    }
 
-void clockwise()
-{
-  for(int i = 7; i >= 0; i--)
-  {
-    setOutput(i);
-    delayMicroseconds(motorSpeed);
+    else if(val == 'd'){
+      forward_A();
+      backward_B();
+    }
+    delay(2000);
   }
-}
-
-void setOutput(int out)
-{
-  digitalWrite(motorPin1, bitRead(lookup[out], 0));
-  digitalWrite(motorPin2, bitRead(lookup[out], 1));
-  digitalWrite(motorPin3, bitRead(lookup[out], 2));
-  digitalWrite(motorPin4, bitRead(lookup[out], 3));
+  else{
+    stop_A_B();
+  }
 }
