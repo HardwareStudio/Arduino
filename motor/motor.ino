e0b741bca9890f0863d7799ec124453cc62da842
ChihMin <s102062111@m102.nthu.edu.tw>
2015-06-24 22:02:46 +0800

    add bluetooth control motor code

--- motor/motor.ino
+++ motor/motor.ino
@@ -3,19 +3,13 @@
 SoftwareSerial BT(12, 11); // recev , trans // recev , trans
 char val;
 
+const byte ENA = 4;
+const byte ENB = 5;
 const byte IN1 = 6;
 const byte IN2 = 7;
 const byte IN3 = 8;
 const byte IN4 = 9;
 
-void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
- // Serial.begin(9600);
-  BT.begin(9600);
-}
 
 void forward_A(){
   digitalWrite(IN1, HIGH);
@@ -38,37 +32,98 @@ void backward_B(){
 }
 
 void stop_A_B(){
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
 }
 
-void loop(){
+void stop_A(){
+  digitalWrite(ENA, LOW);
+}
 
+void start_A(){
+  digitalWrite(ENA, HIGH);
+}
+
+void stop_B(){
+  digitalWrite(ENB, LOW);
+}
+
+void start_B(){
+  digitalWrite(ENB, HIGH);
+}
+
+void start_A_B(){
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+}
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  Serial.begin(9600);
+  BT.begin(9600);
+  BT.println("bluetooth begin !!");
+  BT.println(HIGH);
+}
+
+char last = 'w';
+void loop(){
+  
   if (BT.available()){
     val = BT.read();
+    start_A_B();
     if (val == 'w'){
+      if (last == 's'){
+        stop_A_B();
+        delay(1000);
+        last = 'w';
+        Serial.println("Here is go delay!!");
+      }
+      start_A_B();
       forward_A();
       forward_B();
+      
+      Serial.println("Go forward !!");
     }
     else if (val == 'a'){
-      backward_A();
+      stop_A();
+      start_B();
       forward_B();
+      Serial.println("GO left !!!");
     }
 
     else if (val == 's'){
+      if (last == 'w'){
+        stop_A_B();
+        delay(1000);
+        last = 's';
+      }
+      start_A_B();
       backward_A();
       backward_B();
+      Serial.println("GO back !!!");
     }
 
     else if(val == 'd'){
       forward_A();
-      backward_B();
+      start_A();
+      stop_B();
+      Serial.println("Go right !!!");
     }
-    delay(2000);
+    else{
+      stop_A_B();
+    }
+    //delay(1000);
   }
   else{
-    stop_A_B();
+    //Serial.println("STOP!!!");
+    //stop_A_B();
   }
+
+
 }
